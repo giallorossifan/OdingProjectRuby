0a4bb4bc13a4f994ff8a941fc9c92c652ac9f17e
Claudio Salas <djsalas2133@gmail.com>
2022-12-21 21:41:48 -0800

    added name.rb and rubysandbox.rb files

---
+++ Ruby_SandBox/name.rb
@@ -0,0 +1,14 @@
+puts "what is your name?"
+name = gets.chomp
+puts "what is your last name?"
+last_name = gets.chomp
+
+3.times do
+  puts "great. so your name is " + name + "
+  "+ last_name
+
+
+end
+
+a = 1
+# puts a
